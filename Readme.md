58e5741039b97f1b94b9e66b95528290b34430d6
AsmShuvo <hoffmanhulk99@gmail.com>
2024-08-17 17:52:54 +0600

    add: readme

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Solution for running the server
+
+This Server is created with: `Express`, `Node`, `MongoDB`; 
+
+Getting started
+```
+  git clone https://github.com/AsmShuvo/eat-apple-server.git
+  cd  eat-apple-server
+  npm install
+```
+create a .env file and paste this line
+```
+DB_URL = mongodb+srv://appleUser:XfFew4IblWwzEQLx@cluster0.jgp414t.mongodb.net/appleDB?retryWrites=true&w=majority&appName=Cluster0
+
+```
+now run the command
+```
+  npm start
+```
+
--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config(); 
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.DB_URL);
-    console.log(`Connected to MongoDB Database at ${connection.connection.host}`);
+    const connection = await mongoose.connect(process.env.DB_URL); 
+    console.log(`Connected to MongoDB..!! `);
   } catch (error) {
     console.log("Db connection error: ", error);
-    process.exit(1);
+    process.exit(1);  
   }
 };
 
